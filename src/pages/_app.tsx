17e4fa55b533b4bb1d5d9ec7b81264506a44a1fe
Jonathan <jonathanthomaz96@gmail.com>
2020-10-05 17:44:49 -0300

    initial config project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import GlobalStyles from '../styles/global';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </>
+  );
+};
+
+export default MyApp;
