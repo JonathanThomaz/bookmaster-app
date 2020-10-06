1561a4ac069bba892295ab612f84bd6d8eb9d9a8
Jonathan <jonathanthomaz96@gmail.com>
2020-10-05 22:48:17 -0300

    Create books.ts

---
+++ src/variables/books.ts
@@ -0,0 +1,75 @@
+type book = {
+  id: number;
+  nome: string;
+  autor: string;
+  paginas: string;
+  generos: string;
+  editoras: string;
+  description: string;
+};
+
+export const books: Array<book> = [
+  {
+    id: 1,
+    nome: 'Teste nome',
+    autor: 'Teste nome',
+    paginas: 'Teste nome',
+    generos: 'Teste nome',
+    editoras: 'Teste nome',
+    description: 'Teste nome'
+  },
+  {
+    id: 2,
+    nome: 'Teste nome',
+    autor: 'Teste nome',
+    paginas: 'Teste nome',
+    generos: 'Teste nome',
+    editoras: 'Teste nome',
+    description: 'Teste nome'
+  },
+  {
+    id: 3,
+    nome: 'Teste nome',
+    autor: 'Teste nome',
+    paginas: 'Teste nome',
+    generos: 'Teste nome',
+    editoras: 'Teste nome',
+    description: 'Teste nome'
+  },
+  {
+    id: 7,
+    nome: 'Teste nome',
+    autor: 'Teste nome',
+    paginas: 'Teste nome',
+    generos: 'Teste nome',
+    editoras: 'Teste nome',
+    description: 'Teste nome'
+  },
+  {
+    id: 4,
+    nome: 'Teste nome',
+    autor: 'Teste nome',
+    paginas: 'Teste nome',
+    generos: 'Teste nome',
+    editoras: 'Teste nome',
+    description: 'Teste nome'
+  },
+  {
+    id: 5,
+    nome: 'Teste nome',
+    autor: 'Teste nome',
+    paginas: 'Teste nome',
+    generos: 'Teste nome',
+    editoras: 'Teste nome',
+    description: 'Teste nome'
+  },
+  {
+    id: 6,
+    nome: 'Teste nome',
+    autor: 'Teste nome',
+    paginas: 'Teste nome',
+    generos: 'Teste nome',
+    editoras: 'Teste nome',
+    description: 'Teste nome'
+  }
+];
