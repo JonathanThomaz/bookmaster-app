a094efdb86063bea1268884dbbcacea854af7d11
Jonathan <jonathanthomaz96@gmail.com>
2020-10-05 21:43:38 -0300

    defined layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import GlobalStyles from '../styles/global';
-import { ThemeProvider } from 'styled-components';
-import theme from '../styles/theme';
 import '../styles/antd.less';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <Component {...pageProps} />
-      <GlobalStyles />
-    </ThemeProvider>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import { BookOutlined } from '@ant-design/icons';
-import { Layout } from 'antd';
+import { Button, Layout } from 'antd';
+import Grid from 'antd/lib/card/Grid';
+import ButtonGroup from 'antd/lib/button/button-group';
+
 const { Header, Footer, Content } = Layout;
 
 const Home: React.FC = () => {
@@ -13,8 +16,30 @@ const Home: React.FC = () => {
       <Layout>
         <Header></Header>
         <Content>
-          <h1>Content</h1>
-          <BookOutlined />
+          <Grid>
+            <BookOutlined style={{ fontSize: '16px', color: '#faad14' }} />
+            <p>
+              <span>Nome do livro:</span>
+              <span>Aqui vai o nome do livro</span>
+            </p>
+            <p>
+              <span>Autor:</span>
+              <span>Aqui vai o autor</span>
+            </p>
+            <p>
+              <span>Número de páginas:</span>
+              <span>Aqui vai o numero de paginas</span>
+            </p>
+            <p>
+              <span>Gêneros:</span>
+              <span>Aqui vai os generos</span>
+            </p>
+            <p>
+              <span>Editoras:</span>
+              <span>Aqui vai as editoras</span>
+            </p>
+            <Button type="default" >Ver Descrição</Button>
+          </Grid>
         </Content>
         <Footer>Footer</Footer>
       </Layout>
